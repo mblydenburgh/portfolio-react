cbf8638c55ba0af8e1e4c6d9bc99409b89e35e74
N0364359 <michael.blydenburgh@libertymutual.com>
2019-03-06 12:11:15 -0500

    updated for reaction-movies

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { Selector } from 'testcafe'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+fixture`first test`
+  .page `https://portfolio-mb.herokuapp.com/`
+
+test('contact links open', async t => {
+  await t
+  .click("#root > div > div.sc-bdVaJa.ghTIKQ > ul > li:nth-child(3) > a")
+  .expect(Selector('#github').getAttribute('href'))
+  .eql('https://github.com/mikebly')
+  .expect(Selector('#profile-link').getAttribute('href'))
+  .eql('https://www.freecodecamp.org/mikebly')
+  .expect(Selector('#codepen').getAttribute('href'))
+  .eql("https://codepen.io/mikebly/")
+  .expect(Selector('#linkedin').getAttribute('href'))
+  .eql("https://www.linkedin.com/in/michael-blydenburgh-99a68872/")
+})
+
+test('mobile layout triggers on resize', async t => {
+  await t
+  .resizeWindow(375,667)
+  .expect(Selector('.carousel').visible).ok()
+})
